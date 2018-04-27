84060b7d38b2f745044112eeac21aace402fd412
Clive Walker <clive.walker@cvwdesign.co.uk>
2018-04-27 09:53:08 +0100

    Added files from an older version

---
+++ README.md
@@ -0,0 +1,13 @@
+# cvw_dashboardlinks
+
+This is a Perch CMS dashboard widget that displays a list of links. Links are added by an admin user in the Perch Settings page. Possible uses include:
+
+* A list of links to custom Perch help pages.
+* Link to Google Analytics accounts.
+* A list of links to screencasts that show how Perch works.
+* Links to other areas of Perch admin, for example, link to a specific region/collection for editing.
+* A link to the developer's website.
+
+### Note: 
+
+This is personal project for me to learn about Perch widgets. 
---
+++ perch/addons/apps/cvw_dashboardlinks/dashboard.php
@@ -0,0 +1,27 @@
+<?php
+	$API  = new PerchAPI(1.0, 'cvw_dashboardlinks');
+	$HTML = $API->get('HTML');
+	$Lang  = $API->get('Lang');
+	$Settings = $API->get('Settings');
+	
+	// for later
+	// $url1 = $Settings->get('cvw_dashboardlinks_url1')->settingValue();
+	// $url2 = $Settings->get('cvw_dashboardlinks_url2')->settingValue();
+
+	 ?>
+<div class="widget">
+	<h2>
+    <?php echo $Lang->get('Dashboard Links'); ?>
+	</h2>
+
+	<div class="body">
+		<ul>
+			<li>
+			<a href="<?php echo $Settings->get('cvw_dashboardlinks_url1')->settingValue(); ?>">
+			<?php echo $Settings->get('cvw_dashboardlinks_linktext1')->settingValue(); ?></a></li>
+			<li>
+			<a href="<?php echo $Settings->get('cvw_dashboardlinks_url2')->settingValue(); ?>">
+			<?php echo $Settings->get('cvw_dashboardlinks_linktext2')->settingValue(); ?></a></li>
+		</ul>
+	</div> 
+</div>
